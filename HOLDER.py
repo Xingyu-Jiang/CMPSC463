b3574e7b54addcb22d58cbf6fad257c4866a0ec3
Xingyu-Jiang <xzj5086@psu.edu>
2023-12-02 16:31:42 -0500

    Setups

---
+++ HOLDER.py
@@ -0,0 +1,68 @@
+# Node 1
+node1 = MazeNode()
+node1.set_node_id(1)
+node1.set_decision_node(True)
+
+# Node 2
+node2 = MazeNode()
+node2.set_node_id(2)
+node2.set_decision_node(True)
+
+# Node 3
+node3 = MazeNode()
+node3.set_node_id(3)
+node3.set_decision_node(True)
+
+# Node 4
+node4 = MazeNode()
+node4.set_node_id(4)
+node4.set_decision_node(True)
+
+# Node 5
+node5 = MazeNode()
+node5.set_node_id(5)
+node5.set_decision_node(True)
+
+# Node 6
+node6 = MazeNode()
+node6.set_node_id(6)
+node6.set_decision_node(True)
+
+# Node 7
+node7 = MazeNode()
+node7.set_node_id(7)
+node7.set_decision_node(True)
+
+# Node 8
+node8 = MazeNode()
+node8.set_node_id(8)
+
+# Node 9
+node9 = MazeNode()
+node9.set_node_id(9)
+
+# Node 10
+node10 = MazeNode()
+node10.set_node_id(10)
+
+# Node 11
+node11 = MazeNode()
+node11.set_node_id(11)
+
+# Node 12
+node12 = MazeNode()
+node12.set_node_id(12)
+
+# Node 13
+node13 = MazeNode()
+node13.set_node_id(13)
+
+# Node 14
+node14 = MazeNode()
+node14.set_node_id(14)
+
+# Node 15
+node15 = MazeNode()
+node15.set_node_id(15)
+
+self.root_node = node1
